102ee3955f23d4b26938c6a2b28b238caf2494c4
WilliamWong0709 <williamwongzju@163.com>
2019-02-19 12:15:15 +0800

    hcunl emit

--- mainsite/views.py
+++ mainsite/views.py
@@ -2,5 +2,21 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Post
+from django.template.loader import get_template
+from datetime import datetime
+import sys
+
+reload(sys)
+sys.setdefaultencoding('utf8')
+
 
 # Create your views here.
+
+def homepage(request):
+    template = get_template('index.html')
+    posts = Post.objects.all()
+    now = datetime.now()
+    html = template.render(locals())
+    return HttpResponse(html)
