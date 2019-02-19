f282e1274fa9803da5a53ecd5d487d6ad6ba2fd7
WilliamWong0709 <williamwongzju@163.com>
2019-02-20 00:07:15 +0800

    base header footer

--- mainsite/views.py
+++ mainsite/views.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Post
 from django.template.loader import get_template
@@ -20,3 +20,13 @@ def homepage(request):
     now = datetime.now()
     html = template.render(locals())
     return HttpResponse(html)
+
+def showpost(request, slug):
+    template = get_template('post.html')
+    try:
+        post = Post.objects.get(slug=slug)
+        if post != None:
+            html = template.render(locals())
+            return HttpResponse(html)
+    except:
+        return redirect('/')
